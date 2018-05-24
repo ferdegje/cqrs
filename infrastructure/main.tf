5fade81af48030810f43b9fc0d9998630cbf2b82
Jean-Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2018-05-24 21:30:22 +0100

    Store Terraform state in S3

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-jmf"
+    key    = "github.com/ferdegje/cqrs/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
 module "fargate" {
   source = "./fargate"
 }
