be2c8064e99aebc3141c3f6138704ee3cc08e211
Jean-Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2018-05-24 22:19:13 +0100

    Create task definition with publicly available sample image

--- infrastructure/fargate/cluster.tf
+++ infrastructure/fargate/cluster.tf
@@ -1,3 +1,9 @@
 resource "aws_ecs_cluster" "main" {
   name = "tf-ecs-cluster"
 }
+
+resource "aws_ecs_task_definition" "webservice" {
+  family = "webservice"
+
+  container_definitions = "${file('task_definition.json')}"
+}
